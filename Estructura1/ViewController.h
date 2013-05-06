396573ea382ab0cd24609b6e94206bf19a111f64
Pamela Pacheco <pam_liz31@hotmail.com>
2013-05-06 18:48:33 -0500

    se agrega proyecto al repositorio

---
+++ Estructura1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Estructura1
+//
+//  Created by PamLiiz on 06/05/13.
+//  Copyright (c) 2013 PamLiiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Estructura1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Estructura1
+//
+//  Created by PamLiiz on 06/05/13.
+//  Copyright (c) 2013 PamLiiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
